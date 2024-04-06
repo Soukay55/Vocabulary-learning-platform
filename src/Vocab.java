866dee6248db9f5ec78bf809d5b793b9cd1bdcf2
wisoums <wissem_oumsalem@yahoo.ca>
2024-04-06 14:49:47 -0400

    ca marche

--- src/Vocab.java
+++ src/Vocab.java
@@ -1,3 +1,15 @@
 public class Vocab {
+    private String topic;
 
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Vocab(String topic, String wordName) {
+        this.topic = topic;
+    }
 }
